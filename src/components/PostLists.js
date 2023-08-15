ccbc495ee8c99d9d6f21218e07bd06383dd87b97
damilola <ajay0006@algonquinlive.com>
2023-08-14 21:03:18 -0400

    working on post component

--- src/components/PostLists.js
+++ src/components/PostLists.js
@@ -6,12 +6,18 @@ import {
     deletePost as deletePostApi,
 } from "../api";
 import {Link} from "react-router-dom";
+import {useAsync} from "../hooks/useAsync";
 
 export function PostLists() {
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
-        getPostsApi().then(setPosts)
-    }, []);
+    const {loading, error, value: posts} = useAsync(getPostsApi)
+
+    if (loading) {
+        return <h1> Loading...</h1>
+    }
+    if (loading) {
+        return <h1 className='error-msg'> {error}</h1>
+    }
+
     return posts.map(post => {
         return (
             <h1 key={post.id}>
